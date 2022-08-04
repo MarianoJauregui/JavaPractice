7d0ee2753e8eeadaab3460aa65465096638c7d3a
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-08-04 17:38:43 -0300

    Array and String manipulation

--- src/generalPractices/CodeWars.java
+++ src/generalPractices/CodeWars.java
@@ -1,13 +1,16 @@
 package generalPractices;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CodeWars {
     public static void main(String[] args) {
+
         /*
         Consider an array/list of sheep where some sheep may be missing from their place.
         We need a function that counts the number of sheep present in the array (true means present).
          */
+
         Boolean[] arrayBoolean = {true, true, true, false,
                 true, true, true, true,
                 true, false, true, false,
@@ -44,6 +47,46 @@ public class CodeWars {
             arrayOne[i] *= -1;
         }
         // return arrayOne;
+        /*
+        Return the result of multiplying the values together in order
+         */
+        int product = 1;
+        for (int j : arrayTwo) {
+            product = product * j;
+        }
+
+        /*
+        Grade book: Complete the function so that it finds the average of the three scores
+         passed to it and returns the letter value associated with that grade.
+         These values are hardcoded, in codewars they are introduced with the execution of the program.
+         */
+
+        int s1 = 12;
+        int s2 = 68;
+        int s3 = 91;
+        int score = (int)Math.ceil((s1+s2+s3)/3);
+
+        System.out.println(score>=90 ? 'A'
+                : score>=80 ? 'B'
+                : score>=70 ? 'C'
+                : score>=60 ? 'D' : 'F');
+
+        /*
+        All the animals are having a feast! Each animal is bringing one dish.
+        There is just one rule: the dish must start and end with the same letters as the animal's name.
+        For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+        Write a function feast that takes the animal's name and dish as arguments and returns
+         true or false to indicate whether the beast is allowed to bring the dish to the feast.
+         */
+        String animal = "ALSDFAKSDOFK";
+
+        String firstLetter = animal.substring(0,1);
+        String lastLetter = animal.substring(animal.length() -1);
+
+
+
+
+
 
 
     }
