b3ba955a2c87e65e562aaf93b1063b444eab22de
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-26 14:59:43 -0300

    Codewars - SmileyFaceCount - 6kyu

--- src/generalPractices/codewars/DetectPangram.java
+++ src/generalPractices/codewars/DetectPangram.java
@@ -10,31 +10,10 @@ public class DetectPangram {
     is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
     Given a string, detect whether it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
      */
-    public static boolean check(String sentence){
-        boolean[] alphabeat = new boolean[26];
-        int index = 0;
-        int flag = 1;
 
-        for(int i=0; i<sentence.length(); i++){
-            if(sentence.charAt(i) >= 'A' && sentence.charAt(i) <= 'Z'){
-                index = sentence.charAt(i) - 'A';
-            } else if (sentence.charAt(i) >= 'A' && sentence.charAt(i) <= 'z'){
-                index = sentence.charAt(i) - 'a';
-            }
-            alphabeat[index] = true;
-        }
-        for(int i = 0; i <= 25; i++) {
-            if (!alphabeat[i]) {
-                flag = 0;
-                break;
-            }
-        } if(flag == 1)
-            return true;
-        else return false;
-    }
 
     //Easiest and faster solution
-    public static boolean checkFaster(String sentence){
+    public static boolean check(String sentence){
         for(char c = 'a'; c<= 'z'; c++)
             if(!sentence.toLowerCase().contains("" + c))
                 return false;
