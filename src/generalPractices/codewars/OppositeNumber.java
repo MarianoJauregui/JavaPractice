1f5f1b4fc043b23fb14531b3293103cc86c4b7fb
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-10-05 13:47:01 -0300

    CodeWars - SmallestInteger, OppositeNumber - 8-7 kyu

---
+++ src/generalPractices/codewars/OppositeNumber.java
@@ -0,0 +1,11 @@
+package generalPractices.codewars;
+
+public class OppositeNumber {
+    public static void main(String[] args) {
+        System.out.println(opposite(6));
+    }
+    //Given a number, return it's opposite
+    public static int opposite(int number){
+        return -number;
+    }
+}
