bb2009e2e96a066bac44663f2d93f060ba220bf1
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-11-11 13:43:28 -0300

    INTERVIEW PRACTICE

---
+++ src/main/java/generalPractices/interviews/Number15AndString.java
@@ -0,0 +1,43 @@
+package generalPractices.interviews;
+
+import java.util.Arrays;
+
+/**
+ * * if you have number 15 make 1+5 as output
+ * * String asdfGGkjcl, split it on the doubled symbol and print length of both strings
+ */
+public class Number15AndString {
+    public static void main(String[] args) {
+        System.out.println(intAsNumber(10));
+        splitString("asdfGGksfgthjcl");
+    }
+    public static String intAsNumber(int number){
+        StringBuilder str = new StringBuilder();
+
+        char[] chars = String.valueOf(number).toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            str.append(chars[i]).append("+");
+        }
+
+        return str.substring(0, str.length() -1);
+
+    }
+
+    public static void splitString(String str){
+            String[] out = str.split("(\\w)\\1+");
+            int firstLength = 0;
+            int secondLength = 0;
+        for (int i = 0; i < out.length; i++) {
+            firstLength += out[i].length();
+            i++;
+            secondLength += out[i].length();
+        }
+        System.out.println("Fist half length -> " + firstLength);
+        System.out.println("Second half length -> " + secondLength);
+
+
+        }
+
+    }
+
