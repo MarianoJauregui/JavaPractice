8bb6c3a76b39d6e7c147cbc48578e744cbcb8ce1
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-12-01 08:01:35 -0300

    Interview practice - Optimizing solutions

---
+++ src/main/java/generalPractices/codewars/kyu6/AreTheyTheSame.java
@@ -0,0 +1,44 @@
+package generalPractices.codewars.kyu6;
+
+
+import java.util.Arrays;
+
+/**
+ * Given two arrays a and b write a function comp(a, b) (orcompSame(a, b))
+ * that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+ * "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+ */
+public class AreTheyTheSame {
+
+    public static void main(String[] args) {
+        //These two would be valid arrays. So we would return true.
+        int[] validArr1 = {121, 144, 19, 161, 19, 144, 19, 11};
+        int[] validArr2 = {121, 14641, 20736, 36, 25921, 361, 20736, 361};
+
+        System.out.println(comp(validArr1, validArr2));
+    }
+
+    public static boolean comp(int[] a, int[] b) {
+     if(a ==null || b == null) return false;
+     if(a.length != b.length) return false;
+
+     for(int i = 0; i < b.length; i++){
+         a[i] = a[i]*a[i];
+     }
+     Arrays.sort(a);
+     Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+
+    }
+
+    public static boolean comp2(int[] a, int[] b) { // NOT OPTIMAL
+        if ((a == null) || (b == null)) return false;
+
+        int[] ab = Arrays.stream(a).map(n -> n * n).toArray();
+        Arrays.sort(ab);
+        Arrays.sort(b);
+        return (Arrays.equals(ab, b));
+
+    }
+}
